20f14917a34efb9763bd100d21fb469201d683b6
zacck <coderv63@gmail.com>
2016-02-03 09:33:41 +0200

    Showing Image to user from the internet now to create the logic for the buttons

--- app/src/main/java/com/zacck/androidguessthecelebrity/MainActivity.java
+++ app/src/main/java/com/zacck/androidguessthecelebrity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zacck.androidguessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,15 +10,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView mCelebImage;
     Button bt0,bt1,bt2,bt3;
+    ArrayList<String> ImageLinks = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +43,74 @@ public class MainActivity extends AppCompatActivity {
         DownloadImageTask mDownloadImageTask = new DownloadImageTask();
         try {
             result = mDownloadImageTask.execute("http://www.posh24.com/celebrities").get();
-            Log.i("Url contents are: " , result);
+
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+
+            //find all the bits between 'mi' and 'pi' using regex
+            Pattern webp = Pattern.compile("src=\"(.*?)\"");
+            //matcher
+            Matcher webm = webp.matcher(splitResult[0]);
+
+            while(webm.find())
+            {
+
+                //String[] msplitString = webm.group(1).split("\" alt=\"");
+
+                //System.out.println(Arrays.toString(msplitString));
+                ImageLinks.add(webm.group(1));
+            }
+
+            //find all the bits between 'mi' and 'pi' using regex
+            Pattern namep = Pattern.compile("alt=\"(.*?)\"");
+            //matcher
+            Matcher namem = namep.matcher(splitResult[0]);
+
+            while(namem.find())
+            {
+
+                //String[] msplitString = webm.group(1).split("\" alt=\"");
+
+                //System.out.println(Arrays.toString(msplitString));
+                celebNames.add(namem.group(1));
+            }
+
+            //create a random
+            Random mRandom = new Random();
+            chosenCeleb = mRandom.nextInt(celebNames.size());
+
+            ImageDownloader mImageDownloader = new ImageDownloader();
+            Bitmap celebImage;
+            celebImage = mImageDownloader.execute(ImageLinks.get(chosenCeleb)).get();
+
+            mCelebImage.setImageBitmap(celebImage);
+
+
+
         }
         catch(Exception e)
         {
             e.printStackTrace();
+        }
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>
+    {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL mImageUrl = new URL(urls[0]);
+                HttpURLConnection mImageConnection = (HttpURLConnection)mImageUrl.openConnection();
+                mImageConnection.connect();
+                InputStream ImageStream = mImageConnection.getInputStream();
+                Bitmap mImageBitmap = BitmapFactory.decodeStream(ImageStream);
+                return  mImageBitmap;
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 
@@ -48,20 +121,25 @@ public class MainActivity extends AppCompatActivity {
 
     public class DownloadImageTask extends AsyncTask<String, Void, String>
     {
+        String result = "";
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+           // Log.i("Url contents are: ", result);
+        }
 
         @Override
         protected String doInBackground(String... urls) {
-            String result = "";
+
             URL mURL;
 
             //lets create a connection
             HttpURLConnection mUrlConnection = null;
 
-            try
-            {
+            try {
                 //convert the string passed in into a url
                 mURL = new URL(urls[0]);
-                mUrlConnection = (HttpURLConnection)mURL.openConnection();
+                mUrlConnection = (HttpURLConnection) mURL.openConnection();
                 InputStream ImageStream = mUrlConnection.getInputStream();
                 //make a reader
                 InputStreamReader mInputReader = new InputStreamReader(ImageStream);
@@ -72,8 +150,20 @@ public class MainActivity extends AppCompatActivity {
                     char currentCharacter = (char)data;
                     result += currentCharacter;
                     data = mInputReader.read();
-                    return result;
+
                 }
+                /*
+                BufferedReader buffer = new BufferedReader(mInputReader);
+
+                String s = "";
+
+                while ((s = buffer.readLine()) != null) {
+
+                    result += s;
+
+                }
+                */
+                return result;
 
             }
             catch(Exception e) {
